5d226ed2b3438138713883bcbb28e2ac1b526571
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 15:59:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,32 @@
-class Artist
-  attr_accessor :name, :songs
-  @songs = []
+#class Artist
+#  attr_accessor :name, :songs
+#  @songs = []
 
+#  def initialize(name)
+#    @name = name
+#  end
+
+#  def add_song(songs)
+#    songs.artist = self
+#  end
+
+#  def song_count
+#    @songs.length
+#  end
+#end
+
+class Artist
+  attr_accessor :name
+ 
   def initialize(name)
     @name = name
   end
-
-  def add_song(songs)
-    songs.artist = self
+ 
+  def add_song(song)
+    song.artist = self
   end
-
-  def song_count
-    @songs.length
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 end
