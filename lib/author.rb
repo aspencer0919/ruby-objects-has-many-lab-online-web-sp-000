b26288e57756871e05ec308c53b19292cbbc2051
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 16:58:00 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,6 @@
 class Author
   attr_accessor :name, :posts
 
-  @@post_count = 1
-
   def initialize(name)
     @name = name
     @posts = []
@@ -11,14 +9,12 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
-    @@post_count += 1
   end
 
   def posts
