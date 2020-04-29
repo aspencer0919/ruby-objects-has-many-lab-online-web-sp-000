207d8e28df65ff38d13655ea23d86f2d8ef0d442
Adam Spencer <aspencer0919@gmail.com>
2020-04-29 16:45:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
 class Post
-  attr_accessor :author, :name
+  attr_accessor :author, :title
 
   @@all = []
 
-  def initialize(name, author = nil)
-    @name = name
+  def initialize(title, author = nil)
+    @title = title
     @author = author
     @@all << self
   end
